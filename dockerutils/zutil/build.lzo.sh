29bc3b0ff66e5755549c323882687d797602753c
Eugenio Parodi <ceccopierangiolieugenio@googlemail.com>
2019-04-24 12:45:45 +0100

    Added win64 lzo lib build script

---
+++ dockerutils/zutil/build.lzo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+SHARE_DIR="/tmp/dockershare"
+SHARE_SCRIPT="${SHARE_DIR}/build.lzo.sh"
+LZO_PKG="lzo-2.10"
+
+mkdir -p ${SHARE_DIR}
+
+cat > ${SHARE_SCRIPT} << __END
+#!/bin/bash
+cd /
+apt update
+apt install -y \
+   binutils-mingw-w64 \
+   binutils-mingw-w64-x86-64 \
+   gcc-mingw-w64-base \
+   g++-mingw-w64 \
+   g++-mingw-w64-x86-64 \
+   gcc-mingw-w64-x86-64 \
+   mingw-w64 \
+   mingw-w64-common \
+   build-essential \
+   wget \
+   zip
+
+wget http://www.oberhumer.com/opensource/lzo/download/${LZO_PKG}.tar.gz
+tar xvf *.tar.gz
+
+cd ${LZO_PKG}
+
+./configure \
+    --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 \
+    --prefix='' \
+    --without-pkgconfigdir \
+    --enable-shared
+make
+make install DESTDIR=/out
+
+cd /out
+zip /share/lzo-2.10.x86_64-w64-mingw32.zip . -r -9
+
+chmod 666 /share/*.zip
+__END
+
+docker run -v /tmp/dockershare:/share -i -t ubuntu bash -c "bash /share/build.lzo.sh"
+
+echo "The library is available here: " $(ls ${SHARE_DIR}/*.zip)
+
+
